7db92423ed6ebcd0e5e75369feea0bdf25730477
ecreyes <ereyesoyarzo@gmail.com>
2018-06-13 00:45:36 -0400

    Dom Seguro Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CapitalizadoPipe
+    CapitalizadoPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule
